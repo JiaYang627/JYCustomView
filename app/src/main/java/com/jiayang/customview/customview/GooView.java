cc74f325a070cdfb6601a6205bce93a2ca71492b
JiaYang <JiaYang627@163.com>
2018-05-22 18:13:17 +0800

    两个圆中间区域绘制初始化

--- app/src/main/java/com/jiayang/customview/customview/GooView.java
+++ app/src/main/java/com/jiayang/customview/customview/GooView.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Path;
 import android.graphics.PointF;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
@@ -36,6 +37,24 @@ public class GooView extends View {
     private float dragRadius = 30f;
 
 
+    /**
+     * 固定圆 附着点集合
+     */
+    private PointF[] stablePoints = new PointF[]{
+            new PointF(300f, 300f),
+            new PointF(300f, 400f)
+    };
+
+    /**
+     * 拖拽圆 附着点集合
+     */
+    private PointF[] dragPoints = new PointF[]{
+            new PointF(100f, 300f),
+            new PointF(100f, 400f)
+    };
+
+    private PointF controlPoint = new PointF(200f, 350f);
+    private Path mPath;
 
 
     public GooView(Context context) {
@@ -56,6 +75,7 @@ public class GooView extends View {
         mPaint.setAntiAlias(true);
         mPaint.setColor(Color.RED);
 
+        mPath = new Path();
     }
 
 
@@ -65,5 +85,22 @@ public class GooView extends View {
 
         canvas.drawCircle(stableCenter.x ,stableCenter.y ,stableRadius ,mPaint);
         canvas.drawCircle(dragCenter.x, dragCenter.y, dragRadius, mPaint);
+
+        // 绘制两圆中间的部分
+        // 1 移动Path 到固定圆 附着点1
+        // 2 附着点1 画贝塞尔曲线到 拖拽圆 附着点1
+        // 3 拖拽圆 附着点1 直线 绘制到 拖拽圆 附着点2
+        // 4 拖拽圆 附着点2 画贝塞尔曲线 到固定圆 附着点2
+        // 5 闭合
+
+
+        mPath.moveTo(stablePoints[0].x ,stablePoints[0].y);
+
+        mPath.quadTo(controlPoint.x,controlPoint.y ,dragPoints[0].x ,dragPoints[0].y);
+        mPath.lineTo(dragPoints[1].x ,dragPoints[1].y);
+        mPath.quadTo(controlPoint.x,controlPoint.y ,stablePoints[1].x,stablePoints[1].y);
+
+        mPath.close();
+        canvas.drawPath(mPath, mPaint);
     }
 }
